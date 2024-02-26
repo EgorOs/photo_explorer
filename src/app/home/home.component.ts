f83aaa6b1a9af8c8ff45c7393febbdfc82589364
Egor Osinkin <egoros97@yandex.ru>
2024-02-26 20:15:59 +0400

    Clean up gallery screem

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,34 +18,6 @@ export class HomeComponent implements OnInit {
     plugins: [lgZoom],
   };
 
-  imageLinks: string[] = [
-    'file:///home/egor/Projects/photo_explorer/images/copy_6.jpg',
-    'file:///home/egor/Projects/photo_explorer/images/copy_6.jpg',
-    'file:///home/egor/Projects/photo_explorer/images/copy_6.jpg',
-    'file:///home/egor/Projects/photo_explorer/images/copy_6.jpg',
-    'file:///home/egor/Projects/photo_explorer/images/copy_6.jpg',
-    'https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1406&q=80',
-    'https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1406&q=80',
-    'https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1406&q=80',
-    'https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1406&q=80',
-    'https://images.unsplash.com/photo-1581894158358-5ecd2c518883?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1406&q=80',
-    'https://i.etsystatic.com/19913145/r/il/993892/4865262854/il_570xN.4865262854_awu7.jpg',
-    'https://i1.sndcdn.com/artworks-000105986104-hwbpls-t500x500.jpg',
-    'https://assets.popbuzz.com/2020/40/is-joji-pink-guy-are-they-the-same-person-1602077911-view-0.jpg',
-    'https://i.etsystatic.com/19913145/r/il/993892/4865262854/il_570xN.4865262854_awu7.jpg',
-    'https://i1.sndcdn.com/artworks-000105986104-hwbpls-t500x500.jpg',
-    'https://assets.popbuzz.com/2020/40/is-joji-pink-guy-are-they-the-same-person-1602077911-view-0.jpg',
-    'https://i.etsystatic.com/19913145/r/il/993892/4865262854/il_570xN.4865262854_awu7.jpg',
-    'https://i1.sndcdn.com/artworks-000105986104-hwbpls-t500x500.jpg',
-    'https://assets.popbuzz.com/2020/40/is-joji-pink-guy-are-they-the-same-person-1602077911-view-0.jpg',
-    'https://i.etsystatic.com/19913145/r/il/993892/4865262854/il_570xN.4865262854_awu7.jpg',
-    'https://i1.sndcdn.com/artworks-000105986104-hwbpls-t500x500.jpg',
-    'https://assets.popbuzz.com/2020/40/is-joji-pink-guy-are-they-the-same-person-1602077911-view-0.jpg',
-    'https://i.etsystatic.com/19913145/r/il/993892/4865262854/il_570xN.4865262854_awu7.jpg',
-    'https://i1.sndcdn.com/artworks-000105986104-hwbpls-t500x500.jpg',
-    // Add more image links as needed
-  ];
-
   constructor(private router: Router) { }
 
   ngOnInit(): void {

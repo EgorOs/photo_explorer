42233ace6251fb38339086ef1942c96d8a2ffa27
Egor Osinkin <egoros97@yandex.ru>
2024-02-26 22:09:17 +0400

    Generate thumbnails, because large images exist
    And not only do they exist, but there is plenty of them. So I tested with 500 of 8k images and gallery was freezing badly.
    
    After an hour or so of copy-pasting from one famous LLM we got thumbnails working, yay!

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import lgZoom from 'lightgallery/plugins/zoom';
+import lgThumbnail from 'lightgallery/plugins/thumbnail';
+import { BeforeSlideDetail } from 'lightgallery/lg-events';
 import { ipcRenderer } from 'electron';
 import * as fs from 'fs';
-import { BeforeSlideDetail } from 'lightgallery/lg-events';
+import * as path from 'path';
+import { NgxImageCompressService } from 'ngx-image-compress';
 
 @Component({
   selector: 'app-home',
@@ -15,10 +18,13 @@ export class HomeComponent implements OnInit {
 
   settings = {
     counter: true,
-    plugins: [lgZoom],
+    thumbnail:true,
+    animateThumb: false,
+    showThumbByDefault: false,
+    plugins: [lgZoom, lgThumbnail],
   };
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private imageCompress: NgxImageCompressService) { }
 
   ngOnInit(): void {
     console.log('HomeComponent INIT');
@@ -30,6 +36,7 @@ export class HomeComponent implements OnInit {
   };
 
   images: string[] = [];
+  thumbnails: string[] = [];
 
   loadImagesFromFolder(folderPath: string) {
     fs.readdir(folderPath, (err, files) => {
@@ -38,8 +45,10 @@ export class HomeComponent implements OnInit {
         return;
       }
       this.images = files
-        .filter(file => /\.(jpg|jpeg|png|gif)$/i.test(file)) // Filter image files
-        .map(file => `file://${folderPath}/${file}`); // Convert file paths to URLs
+        .filter(file => /\.(jpg|jpeg|png|gif)$/i.test(file))
+        .map(file => `file://${path.join(folderPath, file)}`);
+      
+        this.resizeImages();
     });
   }
 
@@ -52,4 +61,16 @@ export class HomeComponent implements OnInit {
       }
     });
   }
+
+  async resizeImages() {
+    for (const imageFile of this.images) {
+      try {
+        const resizedImage = await this.imageCompress.compressFile(imageFile, -1, 50, 50, 300);
+        this.thumbnails.push(resizedImage);
+      } catch (error) {
+        console.error('Error resizing image:', error);
+      }
+    }
+  }
+
 }
